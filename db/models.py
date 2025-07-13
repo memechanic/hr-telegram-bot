8b5e435d3f1286e68b1e2fe555470ad426736a32
memechanic <126179346+memechanic@users.noreply.github.com>
2025-07-14 01:05:17 +0500

    Created a model user

---
+++ db/models.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+from sqlalchemy import String, Integer, BigInteger, Boolean
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = "user"
+
+    id : Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+
+    tg_id : Mapped[int] = mapped_column(BigInteger, unique=True, nullable=False, index=True)
+    tg_username : Mapped[Optional[str]] = mapped_column(String(32))
+
+    first_name : Mapped[str] = mapped_column(String(30))
+    last_name : Mapped[str] = mapped_column(String(30))
+    phone_number : Mapped[str] = mapped_column(String(12), unique=True)
+    email : Mapped[str] = mapped_column(String(120), unique=True)
+
+    department : Mapped[Optional[str]] = mapped_column(String(40))
+    position : Mapped[Optional[str]] = mapped_column(String(40))
+
+    is_admin: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
