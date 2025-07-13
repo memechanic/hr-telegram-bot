484cff58658185efe939cd2c92866c407e74ec32
memechanic <126179346+memechanic@users.noreply.github.com>
2025-07-14 01:02:01 +0500

    Added test version of telegram bot

---
+++ app/commands.py
@@ -0,0 +1,6 @@
+from aiogram.types import BotCommand
+
+COMMANDS = [
+    BotCommand(command="start", description="Приветствие и начало работы"),
+    BotCommand(command="dice", description="Бросок кубика")
+]
