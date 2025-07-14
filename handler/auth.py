ed15075cc78da21051dbcc5fbfdd23e69d8bb617
memechanic <126179346+memechanic@users.noreply.github.com>
2025-07-15 02:59:45 +0500

    Corrected misspelled words in russian

--- handler/auth.py
+++ handler/auth.py
@@ -7,7 +7,7 @@ from service.pincode import create_deep_link, is_pincode_right
 
 router = Router()
 
-# авторизация через диплинк
+# авторизация через дип-линк
 @router.message(CommandStart(deep_link=True))
 async def start_pincode(message: Message, command: CommandObject):
     pincode_raw = command.args
@@ -15,9 +15,9 @@ async def start_pincode(message: Message, command: CommandObject):
     if success:
         await message.answer("Успешно авторизирован")
     else:
-        await message.answer("Пинкод использован либо его не существует")
+        await message.answer("Пин-код использован либо его не существует")
 
-# авторизация посредством ввода пинкода пользователем
+# авторизация посредством ввода пин-кода пользователем
 @router.message(CommandStart())
 async def start(message: Message):
     pass
@@ -26,7 +26,7 @@ async def start(message: Message):
 async def create_invite_link(message: Message, bot: Bot):
     link, pincode = await create_deep_link(bot)
     if link:
-        text = f"Сгенерированный пинкод: <code>{pincode}</code>\nCсылка для сотрудника:\n<a>{link}</a>"
+        text = f"Сгенерированный пин-код: <code>{pincode}</code>\nCcылка для сотрудника:\n<a>{link}</a>"
         await message.answer(text, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
     else:
         await message.answer("Что-то пошло не так, попробуйте еще раз")
