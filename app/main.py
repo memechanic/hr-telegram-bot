3d5f5824a82c978b4eda7d40d1085ac532a170ba
memechanic <126179346+memechanic@users.noreply.github.com>
2025-07-19 17:19:49 +0500

    Updated middlewares

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from app.commands import COMMANDS
 from config_reader import config
 from handlers import auth, support
 from handlers.admin import users
-from middlewares import AuthMiddleware, AdminMiddleware
+from middlewares import CheckUserMiddleware
 from locales.loader import reload_locale
 
 dp = Dispatcher()
@@ -21,29 +21,33 @@ async def main():
     bot = Bot(token=config.bot_token.get_secret_value())
 
     # Роутеры для не авторизированных пользователей
-    dp.include_router(auth.router)
+    auth_router = Router()
+    auth_router.include_router(auth.router)
 
     # Роутеры для авторизированных пользователей
     users_router = Router()
     users_router.include_router(support.router)
 
-    users_router.message.middleware(AuthMiddleware())
+    users_router.message.middleware(CheckUserMiddleware("user"))
 
     # Роутеры для администрации
     admin_router = Router()
     admin_router.include_router(users.router)
 
-    admin_router.message.middleware(AdminMiddleware())
+    admin_router.message.middleware(CheckUserMiddleware("admin"))
 
     #Сбор всех роутеров
     dp.include_router(users_router)
     dp.include_router(admin_router)
+    dp.include_router(auth_router)
 
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s::%(levelname)s::%(name)s::%(message)s')
+
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        logging.info("Shutdown requested")
+        logger.info("Shutdown requested")
--- app/middlewares.py
+++ app/middlewares.py
@@ -1,41 +1,33 @@
+import logging
 from typing import Callable, Dict, Any, Awaitable
 
 from aiogram import BaseMiddleware
 from aiogram.types import TelegramObject
+from mako.testing.helpers import result_lines
 
-from service.users import is_user, is_admin
+from service.users import is_user, is_admin, get_user
 
+logger = logging.getLogger(__name__)
 
-class AuthMiddleware(BaseMiddleware):
+class CheckUserMiddleware(BaseMiddleware):
+
+    def __init__(self, who: str):
+        self.who = who
+
     async def __call__(
             self,
             handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
             event: TelegramObject,
             data: Dict[str, Any],
     ) -> Any:
-
         user = data['event_from_user']
-        check = await is_user(user.id)
+        check = await get_user(user.id)
+        result = None
 
-        if check:
-            result = await handler(event, data)
-        else: result = None
+        if self.who == 'user' and check:
+            result = await handler(user, data)
+        elif self.who == 'admin' and check and check.is_admin:
+            result = await handler(user, data)
 
-        return result
-
-class AdminMiddleware(BaseMiddleware):
-    async def __call__(
-            self,
-            handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
-            event: TelegramObject,
-            data: Dict[str, Any],
-    ) -> Any:
-
-        user = data['event_from_user']
-        check = await is_admin(user.id)
-
-        if check:
-            result = await handler(event, data)
-        else: result = None
-
+        logger.debug(f"LoadUserMiddleware: event from user id={user.id}")
         return result
