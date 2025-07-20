ceb6a74c4103766e7a3e1f149b62586dd32340c8
memechanic <126179346+memechanic@users.noreply.github.com>
2025-07-20 20:32:04 +0500

    Добавил отчистку висячих сообщений

--- app/main.py
+++ app/main.py
@@ -41,6 +41,7 @@ async def main():
     dp.include_router(admin_router)
     dp.include_router(auth_router)
 
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
