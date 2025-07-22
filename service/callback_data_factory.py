5aa6f67b13927cfee7694e60dd023542d1fd247a
memechanic <126179346+memechanic@users.noreply.github.com>
2025-07-23 04:13:59 +0500

    Добавил callback data factory

---
+++ service/callback_data_factory.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+from aiogram.filters.callback_data import CallbackData
+
+class MediaTagList(CallbackData, prefix="mtl"):
+    tag: str
+    number: Optional[int] = 0
+    action: Optional[str] = None
+
+# Для будущего изменения в handlers.admin.users при обработке запросов
+# from enum import Enum
+# class RequestAction(str, Enum):
+#     accept = "accept"
+#     decline = "decline"
+#
+# class UserRequestAction(CallbackData, prefix="ura"):
+#     action: RequestAction
+#     user_id: int
