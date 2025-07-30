36d7d99fd4aaea1fc733ff48f044ce1dc50dfbb2
memechanic <126179346+memechanic@users.noreply.github.com>
2025-07-30 16:21:18 +0500

    Добавил создание папок media

--- seed.py
+++ seed.py
@@ -1,13 +1,37 @@
-from db.setup_session import get_async_session
-from db.models import User, StatusEnum
-
-
 import asyncio
+from pathlib import Path
+
+from db.models import User, StatusEnum
+from db.setup_session import get_async_session
+
 """
-    Файл для предварительной настройки - добавление админа в бд
+    Файл для предварительной настройки - добавление админа в бд, а также создание папок для модулей
     Измените данные для добавления админа и запустите данный скрипт
 """
 async def main():
+    media_path = Path(__file__).resolve().parent / 'media'
+    folders = {
+        'cafeteria': media_path / 'cafeteria',
+        'company': media_path / 'company',
+        'docs': media_path / 'docs',
+        'docs_test': media_path / 'docs' / 'docs_test',
+        'structure': media_path / 'structure',
+        'vtours': media_path / 'vtours',
+        'vtours_test': media_path / 'vtours' / 'vtours_test',
+    }
+    try:
+        media_path.mkdir()
+    except Exception as e:
+        print(f"Ошибка создания папки: {e}")
+    else:
+        print("Папка media успешно создана")
+
+    for p in folders.values():
+        try:
+            p.mkdir()
+        except Exception as e:
+            print(f"Ошибка создания папок: {e}")
+    
     async with get_async_session() as session:
         admin = User(
             tg_id = 123456789,              # Для получения айди напишите @check_my_tg_id_bot
